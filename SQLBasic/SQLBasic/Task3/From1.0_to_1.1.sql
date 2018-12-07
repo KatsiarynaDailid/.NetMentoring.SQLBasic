cfc7cd05d3982ed94e8352bc0a21059b028c59cd
KatsairynaDailid <katsiaryna_dailid@epam.com>
2018-12-07 15:18:46 +0300

    Task 3.1 added

---
+++ SQLBasic/SQLBasic/Task3/From1.0_to_1.1.sql
@@ -0,0 +1,20 @@
+﻿--•	1.0 -> 1.1
+--•	Версия 1.0. Основана на оригинальной базе Northwind
+--•	Версия 1.1. Добавляет таблицу данных кредитных карт сотрудников: номер карты, дата истечения, имя card holder, ссылку на сотрудника
+
+if not exists (select * from Sys.Tables SysTables where SysTables.[Name] = 'CreditCards')
+begin
+
+create table [dbo].CreditCards(
+	"CardNumber" "int" IDENTITY (1, 1) NOT NULL ,
+	"ExpirationDate" "datetime" NULL ,
+	"CardHolderName" nvarchar (40) NULL ,
+	"EmployeeID" "int" NULL,
+	CONSTRAINT "FK_CreditCards_Employees" FOREIGN KEY 
+	(
+		"EmployeeID"
+	) REFERENCES "dbo"."Employees" (
+		"EmployeeID"
+	)
+)
+end
---
+++ SQLBasic/SQLBasic/Task3/From1.1_to_1.3.sql
@@ -0,0 +1,19 @@
+﻿--•	Версия 1.1. Добавляет таблицу данных кредитных карт сотрудников: номер карты, дата истечения, имя card holder, ссылку на сотрудника, …
+--•	Версия 1.3. Добавляет следующие минорные изменения относительно 1.1:
+--o	Переименование Region в Regions
+--o	Добавление в таблицу клиентов даты основания
+
+if exists (select * from Sys.Tables SysTables where SysTables.Name = 'Region')
+begin
+
+exec sp_Rename 'dbo.Region', 'Regions';
+
+end
+
+if COL_LENGTH('[dbo].Customers', 'FoundationDate') is null
+begin
+
+alter table [dbo].Customers
+add [FoundationDate] datetime
+
+end
